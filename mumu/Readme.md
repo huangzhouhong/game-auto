4e058356f0118f18948996cfeb30888e8630216f
huangzhouhong <778612084@qq.com>
2025-12-17 19:02:31 +0800

    init commit

---
+++ mumu/Readme.md
@@ -0,0 +1,2 @@
+https://github.com/u-wlkjyy/mumu-python-api?tab=readme-ov-file#gui%E8%87%AA%E5%8A%A8%E5%8C%96%E7%B1%BBauto
+
---
+++ mumu/main.py
@@ -0,0 +1,29 @@
+import sys
+
+from mumu.mumu import Mumu
+
+
+# Mumu(r"C:\Program Files\Netease\MuMu\nx_main\MuMuManager.exe").select(1)
+# Mumu().select(0).androidEvent.screenshot()
+
+
+def handle(frame, mumu):
+    # do something
+    print('接收到模拟器：', mumu.core.utils.get_vm_id(), '的帧',frame.shape)
+    # print(frame)
+    # mumu.auto.save(frame, r'd:\test.png')
+    # sys.exit()
+    pos=Mumu().auto.locateCenterOnScreen(frame,r'd:\fight.png')
+    if pos:
+        print('找到图片中心点：', pos)
+    else:
+        print('未找到图片')
+
+
+Mumu().select(0).auto.create_handle(handle)
+
+
+
+
+
+# print(Mumu().select(0).screen.resolution_mobile())
